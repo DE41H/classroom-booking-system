4f0f797d696bdb488985a89ec43c29a9bfe480a0
sreyash <sreyash.s@outlook.com>
2025-11-01 19:47:39 +0530

    fix: improved error handling and changed Room.view to Room.details

--- bookings.py
+++ bookings.py
@@ -95,130 +95,60 @@ class Menu:
 
     def menu(self) -> None:
         while self.running:
-            try:
                 self.clear()
                 print("\n============= [ MAIN MENU ] =============\n")
                 for option in self.options:
                     print(f'[{option}] {self.options[option]}')
-                choice: str = input("\nEnter Option ID: ")
-                if not choice.isdigit():
-                    raise NaN(choice)
-                option: int = int(choice)
-                func: Callable[[], None] | None = self.functions.get(option)
+                choice: str = self.get_option("Enter Option ID: ", set(str(item) for item in self.options.keys()))
+                func: Callable[[], None] | None = self.functions.get(int(choice))
                 if func:
                     func()
-                else:
-                    raise NotAnOption(choice)
-                break
-            except (NotAnOption, NaN) as err:
-                print(err)
-                sleep(Config.DELAY)
-                
             
     def view(self) -> None:
-        while True:
-            try:
-                self.clear()
-                print("\n============= [ VIEW ROOM ] =============\n")
-                room_no: str = input("Enter Room Number: ")
-                if Room.exists(room_no):
-                    print(Room.view(room_no))
-                    return
-                else:
-                    choice: str = input("Press ENTER to abort...\nEnter text to retry: ")
-                if choice == "":
-                    Menu.running = False
-                else:
-                    raise RoomNotFoundError(room_no)
-            except (RoomNotFoundError) as err:
-                print(err)
-                sleep(Config.DELAY)
-            finally:
-                choice: str = input("Press ENTER to abort...\nEnter text to retry: ")
-                if choice == "":
-                    break
-                else:
-                    continue
+        try:
+            self.clear()
+            print("\n============= [ VIEW ROOM ] =============\n")
+            room_no: str = input("Enter Room Number: ")
+            if Room.exists(room_no):
+                print(Room.details(room_no))
+                input("Press ENTER to continue...")
+            else:
+                raise RoomNotFoundError(room_no)
+        except (RoomNotFoundError) as err:
+            print(err)
+            sleep(Config.DELAY)
 
     def book(self):
-        while True:
-            try:
-                self.clear()
-                print("\n============= [ BOOK ROOM ] =============\n")
-                room_no: str = input("Enter Room Number: ")
-                if not Room.exists(room_no):
-                    raise RoomNotFoundError(room_no)
-                Room.display_hours()
-                hour: int = 0
-                while True:
-                    try:
-                        choice: str = input("Enter booking hour: ")
-                        if not choice.isdigit():
-                            raise NaN(choice)
-                        hour: int = int(choice)
-                        if hour not in range(24):
-                            raise NotAnOption(str(hour))
-                    except (NaN, NotAnOption) as err:
-                        print(err)
-                        sleep(Config.DELAY)
-                    finally:
-                        choice: str = input("Press ENTER to abort...\nEnter text to retry: ")
-                        if choice == "":
-                            break
-                        else:
-                            continue
-                booked: bool = Room.book(room_no, hour)
-                if booked:
-                    return
-                else:
-                    raise TimeslotAlreadyBookedError(room_no, hour)
-            except (RoomNotFoundError, TimeslotAlreadyBookedError) as err:
-                print(err)
-                sleep(Config.DELAY)
-            finally:
-                choice: str = input("Press ENTER to abort...\nEnter text to retry: ")
-                if choice == "":
-                    break
-                else:
-                    continue
+        try:
+            self.clear()
+            print("\n============= [ BOOK ROOM ] =============\n")
+            room_no: str = input("Enter Room Number: ")
+            if not Room.exists(room_no):
+                raise RoomNotFoundError(room_no)
+            print(Room.display_hours())
+            hour: int = int(self.get_option("Enter booking hour", set(str(item) for item in range(24))))
+            booked: bool = Room.book(room_no, hour)
+            if booked:
+                return
+            else:
+                raise TimeslotAlreadyBookedError(room_no, hour)
+        except (RoomNotFoundError, TimeslotAlreadyBookedError) as err:
+            print(err)
+            sleep(Config.DELAY)
                 
-
     def create(self):
-        while True:
-            try:
-                self.clear()
-                print("\n============= [ CREATE ROOM ] =============\n")
-                room_no: str = input("Enter Room Number: ")
-                if Room.exists(room_no):
-                    raise RoomAlreadyExistsError(room_no)
-                building: str = input("Enter Building: ")
-                capacity: str = ""
-                while True:
-                    try:
-                        capacity: str = input("Enter Capacity: ")
-                        if capacity.isdigit():
-                            break
-                        else:
-                            raise NaN(capacity)
-                    except NaN as err:
-                        print(err)
-                        sleep(Config.DELAY)
-                    finally:
-                        choice: str = input("Press ENTER to abort...\nEnter text to retry: ")
-                        if choice == "":
-                            break
-                        else:
-                            continue
-                Room.create(room_no, building, int(capacity))
-            except RoomAlreadyExistsError as err:
-                print(err)
-                sleep(Config.DELAY)
-            finally:
-                choice: str = input("Press ENTER to abort...\nEnter text to retry: ")
-                if choice == "":
-                    break
-                else:
-                    continue
+        try:
+            self.clear()
+            print("\n============= [ CREATE ROOM ] =============\n")
+            room_no: str = input("Enter Room Number: ")
+            if Room.exists(room_no):
+                raise RoomAlreadyExistsError(room_no)
+            building: str = input("Enter Building: ")
+            capacity: int = self.get_int("Enter Capacity: ")
+            Room.create(room_no, building, int(capacity))
+        except RoomAlreadyExistsError as err:
+            print(err)
+            sleep(Config.DELAY)
 
     def find(self):
         while True:
@@ -252,7 +182,7 @@ class Menu:
                             try:
                                 capacity: str = input("Enter required Capacity: ")
                                 if not capacity.isdigit():
-                                    raise NaN(choice)
+                                    raise NaN(capacity)
                                 rooms: set[Room] = Room.by_capacity(int(capacity))
                                 if len(rooms):
                                     print(f'Rooms available with capacity of {rooms}: {rooms}')
@@ -303,6 +233,22 @@ class Menu:
                 else:
                     continue
 
+    @staticmethod
+    def get_int(prompt: str) -> int:
+        x: str = input(prompt)
+        while not x.isdigit():
+            print(NaN(x))
+            x = input(prompt)
+        return int(x)
+    
+    @staticmethod
+    def get_option(prompt: str, options: set[str]):
+        x: str = input(prompt)
+        while not x in options:
+            print(NotAnOption(x))
+            x = input(prompt)
+        return x
+
     @classmethod
     def exit(cls):
         Room.save()
@@ -313,55 +259,53 @@ class Room:
 
     rooms: ClassVar[dict[str, Room]] = {}
 
-    def __init__(self, room_no: str, building: str, capacity: int, booked_hours: list[bool] = [False] * 24) -> None:
+    def __init__(self, room_no: str, building: str, capacity: int, booked_hours: list[bool] | None = None) -> None:
         self.room_no: str = room_no
         self.building: str = building
         self.capacity: int = capacity
-        self.booked_hours: list[bool] = booked_hours
+        self.booked_hours: list[bool] = booked_hours if booked_hours else [False] * 24
 
     def __repr__(self) -> str:
         return f'Room No.: {self.room_no}, Building: {self.building}, Capacity: {self.capacity}'
 
     @classmethod
     def load(cls) -> None:
-        while True:
-            try:
-                with open(Config.BOOKINGS_FILE, "r", newline=Config.NEWLINE) as file:
-                    reader = csv.DictReader(file, delimiter=Config.DELIMITER)
-                    for row in reader:
-                        cls.rooms[row["room_no"]] = Room(row["room_no"], row["building"], int(row["capacity"]), cls.split(row["booked_hours"]))
-                break
-            except FileNotFoundError:
-                print(f'Error: {Config.BOOKINGS_FILE} not found')
-            except (ValueError, csv.Error, KeyError):
-                print(f'Error: Data format error in {Config.BOOKINGS_FILE}')
-            except (IOError, Exception):
-                print(f'Error: Problem loading {Config.BOOKINGS_FILE}')
-            finally:
-                Menu.running = False
+        try:
+            with open(Config.BOOKINGS_FILE, "r", newline=Config.NEWLINE) as file:
+                reader = csv.DictReader(file, delimiter=Config.DELIMITER)
+                for row in reader:
+                    cls.rooms[row["room_no"]] = Room(row["room_no"], row["building"], int(row["capacity"]), cls.split(row["booked_hours"]))
+            return
+        except FileNotFoundError:
+            print(f'Error: {Config.BOOKINGS_FILE} not found')
+        except (ValueError, csv.Error, KeyError):
+            print(f'Error: Data format error in {Config.BOOKINGS_FILE}')
+        except (IOError, Exception):
+            print(f'Error: Problem loading {Config.BOOKINGS_FILE}')
+        finally:
+            Menu.running = False
 
     @classmethod
     def save(cls) -> None:
-        while True:
-            try:
-                os.makedirs(os.path.dirname(Config.BOOKINGS_FILE), exist_ok=True)
-                with open(Config.BOOKINGS_FILE, "w", newline=Config.NEWLINE) as file:
-                    writer = csv.writer(file)
-                    header: list[str] = ["room_no", "building", "capacity", "booked_hours"]
-                    writer.writerow(header)
-                    for room_no, room in cls.rooms.items():
-                        writer.writerow([room_no, room.building, str(room.capacity), cls.join(room.booked_hours)])
-                break
-            except FileNotFoundError:
-                print(f'Error: {Config.BOOKINGS_FILE} not found')
-            except PermissionError:
-                print(f'Error: Lacking permission to write to {Config.BOOKINGS_FILE}')
-            except (ValueError, csv.Error, KeyError):
-                print(f'Error: Data format error in {Config.BOOKINGS_FILE}')
-            except (IOError, Exception):
-                print(f'Error: Problem loading {Config.BOOKINGS_FILE}')
-            finally:
-                Menu.running = False
+        try:
+            os.makedirs(os.path.dirname(Config.BOOKINGS_FILE), exist_ok=True)
+            with open(Config.BOOKINGS_FILE, "w", newline=Config.NEWLINE) as file:
+                writer = csv.writer(file)
+                header: list[str] = ["room_no", "building", "capacity", "booked_hours"]
+                writer.writerow(header)
+                for room_no, room in cls.rooms.items():
+                    writer.writerow([room_no, room.building, str(room.capacity), cls.join(room.booked_hours)])
+            return
+        except FileNotFoundError:
+            print(f'Error: {Config.BOOKINGS_FILE} not found')
+        except PermissionError:
+            print(f'Error: Lacking permission to write to {Config.BOOKINGS_FILE}')
+        except (ValueError, csv.Error, KeyError):
+            print(f'Error: Data format error in {Config.BOOKINGS_FILE}')
+        except (IOError, Exception):
+            print(f'Error: Problem loading {Config.BOOKINGS_FILE}')
+        finally:
+            Menu.running = False
 
     @classmethod
     def create(cls, room_no: str, building: str, capacity: int) -> bool:
@@ -372,7 +316,7 @@ class Room:
             return False
         
     @classmethod
-    def view(cls, room_no: str) -> str:
+    def details(cls, room_no: str) -> str:
         out: str = ""
         room: Room = cls.rooms[room_no]
         out += "Room Number => " + room.room_no + "\n"
